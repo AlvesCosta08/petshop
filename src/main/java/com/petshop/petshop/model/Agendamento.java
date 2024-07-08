2b2dd842ecbbcb504af2c6405c713021e9c2e16b
Alves <alvescosta74@gmail.com>
2024-07-08 14:08:01 -0300

    Criado todas as entidade e mapeadas

--- src/main/java/com/petshop/petshop/model/Agendamento.java
+++ src/main/java/com/petshop/petshop/model/Agendamento.java
@@ -26,54 +26,6 @@ public class Agendamento {
     @JoinColumn(name = "cliente_id", nullable = false)
     private Cliente cliente;
 
-    public Agendamento() {
-    }
 
-    public Agendamento(Long id, LocalDateTime dataHora, Servico servico, Pet pet, Cliente cliente) {
-        this.id = id;
-        this.dataHora = dataHora;
-        this.servico = servico;
-        this.pet = pet;
-        this.cliente = cliente;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getDataHora() {
-        return dataHora;
-    }
-
-    public void setDataHora(LocalDateTime dataHora) {
-        this.dataHora = dataHora;
-    }
-
-    public Servico getServico() {
-        return servico;
-    }
-
-    public void setServico(Servico servico) {
-        this.servico = servico;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
 }
