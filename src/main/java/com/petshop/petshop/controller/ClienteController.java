fab52f3112d2e4fab64d6c4ef4b2c3b1b29c5a5e
Alves <alvescosta74@gmail.com>
2024-07-09 11:11:20 -0300

    corrgir error ao salvar um cliente

--- src/main/java/com/petshop/petshop/controller/ClienteController.java
+++ src/main/java/com/petshop/petshop/controller/ClienteController.java
@@ -7,24 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
+
     @Autowired
     private ClienteService service;
 
     @GetMapping("/cadastrar")
-    public String cadastrar(Cliente cliente) {
-        return "cliente/cadastro";
-    }
-
-    @GetMapping("/cliente/cadastro")
-    public String showForm(Model model) {
+    public String cadastrar(Model model) {
         model.addAttribute("cliente", new Cliente());
         return "cliente/cadastro";
     }
@@ -36,9 +31,20 @@ public class ClienteController {
     }
 
     @PostMapping("/salvar")
-    public String salvar(Cliente cliente, RedirectAttributes attr) {
+    public String salvar(@ModelAttribute Cliente cliente, BindingResult result, RedirectAttributes attr) {
+        if (result.hasErrors()) {
+            return "cliente/cadastro";
+        }
         service.create(cliente);
         attr.addFlashAttribute("success", "Cliente inserido com sucesso.");
-        return "redirect:/clientes/cadastrar";
+        return "redirect:/clientes/listar";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+        service.delete(id);
+        attr.addFlashAttribute("success", "Cliente excluído com sucesso.");
+        return "redirect:/clientes/listar";
     }
 }
+
