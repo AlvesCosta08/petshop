d9a0ac6fd795f8711f04e957061f9badec813a31
Alves <alvescosta74@gmail.com>
2024-07-13 16:10:31 -0300

    Ajustes sistema de login

--- src/main/java/com/petshop/petshop/controller/DashboardController.java
+++ src/main/java/com/petshop/petshop/controller/DashboardController.java
@@ -1,14 +1,22 @@
 package com.petshop.petshop.controller;
 
 
+import com.petshop.petshop.model.Usuario;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
     @GetMapping("/dashboard")
-    public String showDashboard() {
-        return "dashboard";
+    public String dashboardPage(HttpSession session, Model model) {
+        Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
+        if (usuario == null) {
+            return "redirect:/login"; // Redirecionar se não estiver logado
+        }
+        model.addAttribute("usuario", usuario);
+        return "dashboard"; // Retornar a view do dashboard
     }
 }
