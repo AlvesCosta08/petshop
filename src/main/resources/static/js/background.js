2ebb94da544d317a05b80da103957ea295ea3702
Alves <alvescosta74@gmail.com>
2024-07-16 08:33:02 -0300

    criato autnticação d usuarios

--- src/main/resources/static/js/background.js
+++ src/main/resources/static/js/background.js
@@ -1,25 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var images = [
-        'css/images/dog01.jpeg',
-        'css/images/dog02.jpeg',
-        'css/images/dog03.jpg',
-        'css/images/dog04.jpg',
-        'css/images/dog05.jpg',
-        'css/images/dog06.jpg',
-        'css/images/dog07.jpg',
-        'css/images/dog08.jpeg',
-        'css/images/dog09.jpg'
-    ];
+    let page = 0; // Inicializa a página
+    const size = [[${produtos.size()}]]; // Usando Thymeleaf para passar o tamanho da página
+    let loading = false;
 
-    var intervalTime = 5000; // Intervalo de 5 segundos (5000 milissegundos)
-    var bodyElement = document.body;
-
-    function changeBackground() {
-        var randomImage = images[Math.floor(Math.random() * images.length)];
-        bodyElement.style.backgroundImage = 'url(' + randomImage + ')';
-    }
-
-    // Iniciar mudança de imagem automaticamente e repetir a cada intervalTime
-    changeBackground(); // Primeira mudança imediata ao carregar
-    setInterval(changeBackground, intervalTime);
-});
+    window.addEventListener('scroll', () => {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 500 && !loading) {
+            loading = true;
+            document.getElementById('loading').style.display = 'block';
+            fetch(`/produtos?page=${page}&size=${size}`)
+                .then(response => response.text())
+                .then(html => {
+                    document.getElementById('produtos-container').insertAdjacentHTML('beforeend', html);
+                    page++; // Incrementa a página
+                    loading = false;
+                    document.getElementById('loading').style.display = 'none';
+                })
+                .catch(error => {
+                    console.error('Erro ao carregar produtos:', error);
+                    loading = false;
+                    document.getElementById('loading').style.display = 'none';
+                });
+        }
+    });
